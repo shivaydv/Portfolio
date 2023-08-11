22c82cf4b7ec1a3890ad1e321f0a9461e7d94e08
Shiva Yadav <shivaydv1256@gmail.com>
2023-08-11 17:58:54 +0530

    Refactore the progressbar code

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Home from './Pages/Home'
 import Contact from './Pages/Contact'
 import Navbar from './Components/Navbar'
+import Progressbar from "./Components/Progressbar"
 
 const App = () => {
   return (
-    <Router >
+    <Router>
+      <Progressbar/>
       <Navbar/>
       <Routes>
       <Route path='/' element={<Home/>} />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeroSection from "../Components/HeroSection";
 import Project from "../Components/Project";
-import Progressbar from "../Components/Progressbar";
+
 import GoToTop from "../Components/GoToTop";
 
 
@@ -9,7 +9,7 @@ const Home = () => {
 
   return (
     <div className="container mx-auto overflow-x-hidden ">
-      <Progressbar/>
+     
       <GoToTop/>
       <HeroSection/>
       <Project/>
